9f4afae16b6ffd632dde8c94098038cf699da1d7
Kraig Strong <kstrong@tripwire.com>
2018-08-08 13:24:38 -0700

    added a route for adding players

--- build/index.js
+++ build/index.js
@@ -6,15 +6,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const koa_1 = __importDefault(require("koa"));
 const koa_bodyparser_1 = __importDefault(require("koa-bodyparser"));
 const db_1 = require("./db");
+const koa_router_1 = __importDefault(require("koa-router"));
+const router = new koa_router_1.default();
 const app = new koa_1.default();
 const db = new db_1.Db();
-start();
-app.use(koa_bodyparser_1.default());
-app.use(async (ctx, next) => {
-    await next();
+router.post('/api/player', async function addPlayer(ctx, next) {
+    // ctx.router available
+    console.log(ctx.request.body);
+    var fields = ctx.request.body;
+    if (!fields) {
+        // invalid stuff!
+        ctx.status = 400;
+        ctx.body = { error: "empty fields" };
+        return;
+    }
+    var player_name = fields.name;
+    delete fields.name;
+    await db.write(player_name, fields);
+    ctx.status = 200;
+    ctx.body = { update: "update successful" };
+});
+router.get('/', async function home(ctx, next) {
     ctx.body = `
     <h1>hello world</h1>
-    <form method="POST">
+    <form method="POST" action="/api/player">
     Player Name: <input type="text" name="name"/> <br/>
     Speed: <input type="text" name="speed"/> <br/>
     On Base: <input type="text" name="onBase"/> <br/>
@@ -23,19 +38,18 @@ app.use(async (ctx, next) => {
     `;
     ctx.headers["content-type"] = "text/html";
 });
-app.use(async (ctx, next) => {
+app
+    .use(koa_bodyparser_1.default())
+    .use(async (ctx, next) => {
+    debugger;
     await next();
-    if (ctx.request.method == "POST") {
-        console.log(ctx.request.body);
-        var fields = ctx.request.body;
-        var player_name = fields.name;
-        delete fields.name;
-        await db.write(player_name, fields);
-    }
-});
-app.listen(3000);
+})
+    .use(router.routes());
+//.use(router.allowedMethods());
 async function start() {
     console.log("starting");
     await db.init();
+    app.listen(3000);
 }
+start();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
 import Koa from "koa"
 import bodyParser from "koa-bodyparser"
 import {Db} from "./db"
+import Router from "koa-router"
 
+const router = new Router()
 const app = new Koa()
 const db = new Db()
-start()
-app.use(bodyParser())
-app.use( async (ctx, next) => {
-    await next()
+
+router.post('/api/player', async function addPlayer (ctx, next) {
+    // ctx.router available
+    console.log(ctx.request.body)
+    var fields: any = ctx.request.body
+    if (!fields){
+        // invalid stuff!
+        ctx.status = 400
+        ctx.body = {error: "empty fields"}
+        return
+    }
+    var player_name =  fields.name
+    delete  fields.name
+    await db.write(player_name, fields)
+    ctx.status = 200
+    ctx.body =  {update: "update successful"}
+});
+router.get('/', async function home (ctx, next) {
     ctx.body = `
     <h1>hello world</h1>
-    <form method="POST">
+    <form method="POST" action="/api/player">
     Player Name: <input type="text" name="name"/> <br/>
     Speed: <input type="text" name="speed"/> <br/>
     On Base: <input type="text" name="onBase"/> <br/>
@@ -19,18 +35,19 @@ app.use( async (ctx, next) => {
     `
     ctx.headers["content-type"] = "text/html"
 })
-app.use( async (ctx, next) => {
-    await next()
-    if (ctx.request.method == "POST") {
-        console.log(ctx.request.body)
-        var fields = ctx.request.body
-        var player_name =  fields.name
-        delete  fields.name
-        await db.write(player_name, fields)
-    }
-})
-app.listen(3000)
+
+app
+    .use(bodyParser())
+    .use(async (ctx, next) => {
+        debugger
+        await next()
+    })
+    .use(router.routes())
+    //.use(router.allowedMethods());
+
 async function start(){
     console.log("starting")
     await db.init()
+    app.listen(3000)
 }
+start()
