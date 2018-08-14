2d417d942e92016f79bc3d2fdb14de105161466f
Kraig Strong <kstrong@tripwire.com>
2018-08-14 13:11:47 -0700

    start writing objects to the database

--- src/db_load.ts
+++ src/db_load.ts
@@ -1,22 +1,32 @@
+import {Db} from "./db"
+
+const db = new Db()
+
 var fs = require('fs'),
     readline = require('readline');
 
-var rd = readline.createInterface({
-    input: fs.createReadStream('../raw_data/batters.csv'),
-    console: false
-});
+async function start(){
+    console.log("starting db")
+    await db.init()
 
-rd.on('line', function(line: String) {
-    const parts = line.split(',')
-    var name = parts[0]
-    var year = parts[1]
-    var set = parts[2]
-    var number = parts[3]
-    var unique_id = `${year}-${set}-${number}`
-    var team = parts[4]
-    var on_base = parts[5]
-    if (name == "Ken Griffey Jr.") {
-        console.log(`Ken was on ${team} in ${year}`)
-    }
+    var rd = readline.createInterface({
+        input: fs.createReadStream('../raw_data/batters.csv'),
+        console: false
+    });
+    
+    rd.on('line', function(line: String) {
+        const parts = line.split(',')
+        var [name, year] = parts
+        var set = parts[2]
+        var number = parts[3]
+        var unique_id = `${year}-${set}-${number}`
+        var team = parts[4]
+        var on_base = parts[5]
+    
+        if (name == "Ken Griffey Jr.") {
+            db.write(unique_id, `{team: ${team}, on_base: ${on_base}}`)
+        }
+    });
+}
 
-});
+start()
